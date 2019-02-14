ee3e632e710a1097d3a1acdb8d395ee85ad611e4
Nbiba Bedis <bedisnbiba@gmail.com>
2019-02-14 01:12:38 +0100

    clean up

--- src/limit.rs
+++ src/limit.rs
@@ -2,25 +2,10 @@ use std::collections::HashMap;
 use std::fs::File;
 use std::io::Write;
 use std::process::Command;
-use std::sync::mpsc;
 use std::sync::Arc;
 use std::sync::Mutex;
 use std::thread;
 
-/*fn main() {
-    let (program, download, upload) = parse();
-    limit(program, download, upload);
-}*/
-
-/*fn parse() -> (String, String, Option<String>) {
-    let cmd_args: Vec<String> = args().skip(1).collect();
-    let upload = if cmd_args.len() == 4 {
-        Some(cmd_args[3].clone())
-    } else {
-        None
-    };
-    (cmd_args[0].clone(), cmd_args[1].clone(), upload)
-}*/
 pub struct Limiter {
     state: Arc<Mutex<HashMap<String, String>>>,
 }
@@ -33,7 +18,6 @@ impl Limiter {
     pub fn limit(&mut self, p: &str, d: String, u: Option<String>) {
         let p = p.to_owned();
         let p1 = p.clone();
-        //let (tx, rx) = mpsc::channel();
         let state = Arc::clone(&self.state);
         thread::spawn(move || {
             stop();
@@ -60,7 +44,6 @@ impl Limiter {
 
             Self::execute(state);
         });
-        //let shell = rx.try_recv().unwrap();
     }
 
     fn execute(state: Arc<Mutex<HashMap<String, String>>>) {
@@ -105,19 +88,12 @@ pub fn list_process() -> Vec<String> {
         })
         .collect();
 
-    // remove duplicates
-    //--------------
-    //let set: HashSet<_> = process_list.drain(..).collect(); // dedup
-    //process_list.extend(set.into_iter());
-    //--------------
-
     process_list.sort_by(|(_, k), (_, k2)| {
         k.parse::<f32>()
             .unwrap()
             .partial_cmp(&k2.parse::<f32>().unwrap())
             .unwrap()
     });
-    //dbg!(&process_list);
 
     process_list.into_iter().map(|(p, _)| p).rev().collect()
 }
@@ -127,9 +103,3 @@ pub fn stop() {
         .output()
         .unwrap();
 }
-
-/*fn which(p: String) -> String {
-    String::from_utf8(Command::new("which").arg(&p).output().unwrap().stdout).unwrap()
-}*/
-
-// tw firefox 300
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,14 @@
 use gtk::*;
-//use std::process::Command;
 use std::cell::RefCell;
 use std::rc::Rc;
 
 mod limit;
 use limit::{list_process, stop, Limiter};
 
-//const COL: i32 = 0;
-//const MARGIN: i32 = 200;
 fn main() {
     gtk::init().unwrap();
 
-    let mut limiter = Limiter::new();
+    let limiter = Limiter::new();
     let ref_limiter = Rc::new(RefCell::new(limiter));
     let process = list_process();
 
@@ -23,7 +20,7 @@ fn main() {
         let hbox = create_box("h");
         let btn = SpinButton::new_with_range(0.0, 1000.0, 100.0);
         let p2: String = p.clone().to_owned();
-        let mut lc = ref_limiter.clone();
+        let lc = ref_limiter.clone();
         btn.connect_value_changed(move |btn| {
             lc.borrow_mut()
                 .limit(&p2, btn.get_value().to_string(), None);
